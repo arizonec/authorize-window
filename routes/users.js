f9fb03de7c16c389b0d26e4b84ea22936d708234
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-09-24 23:06:47 +0300

    login and register responces functions

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const { login, register, current } = require('../controllers/users')
+
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.post('/login', login);
+router.post('/register', register);
+router.get('/current', current);
+
+module.exports = router;
